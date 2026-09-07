39012b9ea9ae1d26f59321ebf027a5898369a87a
Arsyacoo <arsyalavina@gmail.com>
2026-09-07 09:46:43 +0700

    build(android): initialize capacitor android foundation

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.arsyacoo.sholatku',
+  appName: 'Sholatku',
+  webDir: 'dist-mobile',
+};
+
+export default config;
